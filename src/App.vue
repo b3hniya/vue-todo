3efe95a35795868fe69d6ceac6cb8a846b31395c
b3hniya <b3hniya@gmail.com>
2021-05-12 22:39:23 +0100

    style files had some improvement.(read message)
    styles moved from app.vue to global.sass.
    a new variable called fonts added to variables folder.
    all fonts and colors added to typography file and a bug that caused by * selector with font-family property fixed.

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<style lang="sass">
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap')
-
-*
-  font-family: 'Open Sans', sans-serif !important
-
-body
-  margin: 0
-</style>
