3488f14e0c0380877f9c566507ad98ec1395afb9
b3hniya <b3hniya@gmail.com>
2021-05-12 12:03:36 +0100

    Project initialized and its ready to develop.
    mdi-vue added for icons.
    global.sass file added to vue.config.json for using style file globaly.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap')
 
-#nav {
-  padding: 30px;
+*
+  font-family: 'Open Sans', sans-serif !important
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+body
+  margin: 0
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mdiVue from 'mdi-vue/v2'
+import * as mdijs from '@mdi/js'
 
 Vue.config.productionTip = false
+
+Vue.use(mdiVue, {
+  icons: mdijs
+}) 
 
 new Vue({
   router,
