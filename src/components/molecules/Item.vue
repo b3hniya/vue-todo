b75f975e51fe6728f7e998536bf0bfba7f919517
b3hniya <b3hniya@gmail.com>
2021-05-13 17:24:58 +0100

    two new components added to molecules folder.
    ListItem => it will passes an unordered list.
    Item     => it will passes li element. it is molecule because it can take more than one components.

---
+++ src/components/molecules/Item.vue
@@ -0,0 +1,14 @@
+<template>
+  <li class="item">
+    <slot> </slot>
+  </li>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass">
+.item
+    list-style-type: none
+</style>
