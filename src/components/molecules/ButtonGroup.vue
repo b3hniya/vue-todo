6bc3fcb6200ff0a9e4c925a9954fbe2a8920cc14
b3hniya <b3hniya@gmail.com>
2021-05-13 16:25:08 +0100

    ButtonGroup component added.
    it is a molecule and takes multiple btn component.
    it takes 2 attributes :
    1. vertical   => orders ButtonGroup as a vertical unit.
    2. horizontal => orders ButtonGroup as a horizontal unit.(default)

---
+++ src/components/molecules/ButtonGroup.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul id="button-group" :class="classBinder()">
+    <slot></slot>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    vertical: {
+      type: Boolean,
+      default: false,
+    },
+
+    horizontal: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  methods: {
+    classBinder() {
+      let cls = "";
+
+      cls += this.adjustOrder();
+
+      return cls;
+    },
+
+    adjustOrder() {
+      if (this.vertical) return " vertical";
+      if (this.horizontal) return " horizontal";
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+.vertical
+  @extend .column
+
+.horizontal
+  @extend .row
+</style>
