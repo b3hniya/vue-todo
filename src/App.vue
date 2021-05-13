49c86bf3d42109fdc43563569a6f2ad308648b05
b3hniya <b3hniya@gmail.com>
2021-05-13 20:30:00 +0100

    Navbar component compeleted and added to app file.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <navbar />  
     <router-view />
   </div>
 </template>
---
+++ src/components/organisms/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navbar">
+    <list-item>
+      <item class="py-4">
+        <mdicon name="vuejs" />
+      </item>
+
+      <button-group vertical>
+        <btn icon class="my-1">
+          <mdicon name="vuejs" />
+        </btn>
+        <btn icon class="my-1">
+          <mdicon name="vuejs" />
+        </btn>
+        <btn icon class="my-1">
+          <mdicon name="vuejs" />
+        </btn>
+      </button-group>
+
+      <item class="my-4">
+        <btn icon>
+          <mdicon name="vuejs" />
+        </btn>
+      </item>
+    </list-item>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass">
+.navbar
+    width: 64px
+    height: 100vh
+    background-color: $secondary-light
+    .list-item
+        height: 100vh
+        @extend .column
+        @extend .justify-between
+        align-items: center
+</style>
