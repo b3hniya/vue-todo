7562faebca45fb941d6a6267c4fd576f0e272288
b3hniya <b3hniya@gmail.com>
2021-05-16 13:52:26 +0100

    Appbar component added to organisms folder.

---
+++ src/components/organisms/Appbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1
+    class="page-title"
+    :class="scrollPosition > 80 ? ' scroll-page-title' : ''"
+  >
+    <slot />
+  </h1>
+</template>
+
+<script>
+export default {
+  props: ["scrollPosition"],
+};
+</script>
+
+<style lang="sass">
+.page-title
+  z-index: 0
+  width: 100%
+  position: fixed
+  font-weight: 900
+  box-shadow: none
+  padding-top: 64px
+  @extend .px-4
+  background-color: #f0f0f0
+  transition: all 0.2s ease
+
+.scroll-page-title
+  padding-bottom: 32px
+  box-shadow: 0 1px 5px gray
+  transform: translate(0px, -32px)
+</style>
