8adbdb30ef863d789aad5273e22cdfe90ed0b780
b3hniya <b3hniya@gmail.com>
2021-05-16 20:18:32 +0100

    BigTag component added to atoms folder.
    it contains tag name and two different icon.
    one fore deleting em and one for adding em.

---
+++ src/components/atoms/BigTag.vue
@@ -0,0 +1,32 @@
+<template>
+  <p class="text-lg big-tag">
+    <slot />
+    <mdicon name="close" v-if="close" />
+    <mdicon name="plus" v-else />
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    close: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+.big-tag
+    @extend .px-2
+    @extend .py-1
+    @extend .row
+    border-radius: 50px
+    @extend .justify-between
+    background-color: $primary
+    color: $main-light !important
+    span
+      margin-left: 16px
+      @include main-hover-anim
+</style>
