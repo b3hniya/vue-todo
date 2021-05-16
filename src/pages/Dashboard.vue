128922f81971e028167a6cc4eba621e670a06070
b3hniya <b3hniya@gmail.com>
2021-05-16 14:05:57 +0100

    Dashboard component adjusted and ready to work.

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>test</h1>
+  <div class="dashboard" v-on:scroll="updateScroll">
+    <appbar :scrollPosition="scrollPosition">Dashboard</appbar>
+
+    <important-task class="response-to-side" />
+
+    <recently-task class="response-to-side" />
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    scrollPosition: 0,
+  }),
+
+  methods: {
+    updateScroll(e) {
+      this.scrollPosition = e.srcElement.scrollTop;
+    },
+  },
+};
 </script>
 
-<style></style>
+<style lang="sass">
+.response-to-side
+  @include response-to-size
+
+.dashboard
+  height: 100vh
+  width: 100vw
+  @extend .column
+  overflow-x: hidden
+</style>
